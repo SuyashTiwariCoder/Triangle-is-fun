0c8f59a46bfa7b7d06edb3d6a3d933b36bbbf01f
SuyashTiwariCoder <89460164+SuyashTiwariCoder@users.noreply.github.com>
2021-09-12 10:45:10 +0530

    hypo done

--- trihypo.js
+++ trihypo.js
@@ -1,15 +1,16 @@
 let input_of_sides = document.querySelectorAll(".sides-input");
 
 let checkBtn = document.querySelector("#check-btn");
-let output = document.querySelector("#output")
+let output = document.querySelector("#output");
 checkBtn.addEventListener("click", calculateHypo);
 
 function calculateHypo() {
-  let c =calculateSumOfSquares(Number(input_of_sides[0].value),
-   (Number(input_of_sides[1].value)));
- let hypoLength = Math.sqrt(c);
- output.innerText = "The Hypotenuse of triangle is "+ hypoLength
- 
+  let c = calculateSumOfSquares(
+    Number(input_of_sides[0].value),
+    Number(input_of_sides[1].value)
+  );
+  let hypoLength = Math.sqrt(c);
+  output.innerText = "The Hypotenuse of triangle is " + hypoLength;
 }
 
 function calculateSumOfSquares(a, b) {
