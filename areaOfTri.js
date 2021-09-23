aab422cc25e0211eba19ac2c5adb0e82470ffc87
SuyashTiwariCoder <89460164+SuyashTiwariCoder@users.noreply.github.com>
2021-09-23 22:32:11 +0530

    formatting

--- areaOfTri.js
+++ areaOfTri.js
@@ -1,24 +1,23 @@
-let inputBase = document.querySelector(".inputBase")
-let inputHeight = document.querySelector(".inputHeight")
-let calculateBtn = document.querySelector("#calculateArea")
-let output = document.querySelector("#output")
+let inputBase = document.querySelector(".inputBase");
+let inputHeight = document.querySelector(".inputHeight");
+let calculateBtn = document.querySelector("#calculateArea");
+let output = document.querySelector("#output");
 
-calculateBtn.addEventListener('click' , isPositive)
+calculateBtn.addEventListener("click", isPositive);
 
 function calculateArea() {
-    let area = 1/2*((Number(inputBase.value)) * (Number(inputHeight.value)))
-    output.innerText = "The area of Tringle is =>" + area
+  let area = (1 / 2) * (Number(inputBase.value) * Number(inputHeight.value));
+  output.innerText = "The area of Tringle is =>" + area;
 }
 
 function isPositive() {
-    if (Number(inputBase.value) >=0 ) {
-       if (Number(inputHeight.value) >=0) {
-           calculateArea()
-           
-       } else {
-        alert("Height cannot be negative")
-       }
+  if (Number(inputBase.value) >= 0) {
+    if (Number(inputHeight.value) >= 0) {
+      calculateArea();
     } else {
-        alert("Base cannot be negative")
+      alert("Height cannot be negative");
     }
+  } else {
+    alert("Base cannot be negative");
+  }
 }
